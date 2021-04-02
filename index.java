e004bf10d0a0b9a98307e5f6ee918bdfbb0b8a57
“gk2533” <gk2533@columbia.edu>
2021-04-02 18:41:25 -0400

    Finished with the structure, 10x10 works, set to Test Case 1

--- index.java
+++ index.java
@@ -9,7 +9,7 @@ public class index {
       public static void main(String args[])
       {
           //Creates town, prints First Generation
-          int[][] arr = new int[5][5];
+          int[][] arr = new int[10][10];
           createTown(arr);
           System.out.println("\n\n0th Generation Town: \n");
           printTown(arr);
@@ -143,13 +143,36 @@ public class index {
           }
         }
 
-        arr[0][2] = 1;
-        arr[1][2] = 1;
-        arr[1][3] = 1;
-        arr[2][1] = 2;
-        arr[2][2] = 2;
-        arr[2][3] = 1;
-        arr[3][3] = 1;
+        arr[0][2] = 2;
+        arr[0][3] = 2;
+        arr[1][4] = 1;
+
+        arr[1][5] = 3;
+        arr[1][6] = 1;
+
+        arr[2][3] = 2;
+        arr[2][5] = 3;
+        arr[2][8] = 1;
+        arr[2][9] = 3;
+
+        arr[3][4] = 1;
+        arr[3][5] = 3;
+        arr[3][9] = 3;
+
+        arr[4][1] = 2;
+        arr[4][2] = 2;
+        arr[4][6] = 1;
+        arr[4][7] = 3;
+        arr[4][8] = 1;
+
+        arr[5][5] = 2;
+        arr[5][6] = 2;
+
+        arr[6][2] = 2;
+        arr[6][4] = 2;
+
+        arr[7][4] = 2;
+
       }
 
       //Description: Simple method that prints out entire town
